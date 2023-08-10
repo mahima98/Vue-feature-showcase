ec34d522f36850022a2f67966c3ab3b9cec3fe04
Mahima Ramgolam <mahima@bazaks.com>
2023-08-10 21:30:50 +0400

    created create get and post api through pinia store

--- pages/patients/index.vue
+++ pages/patients/index.vue
@@ -1,66 +1,53 @@
 <script setup>
 import { usePatientStore } from "@/store/patient/patientStore";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
 
 const patientStore = usePatientStore();
 const { PatientList, getPatientList } = usePatientStore();
+// getPatientList();
 
-// patientStore.getAllPatients();
-getPatientList();
+const { errors, handleSubmit, defineInputBinds } = useForm({
+  validationSchema: yup.object({
+    // email: yup.string().email().required(),
+    // password: yup.string().min(6).required(),
+  }),
+});
 
-console.log("patientList");
+// Creates a submission handler
+// It validate all fields and doesn't call your function unless all fields are valid
+const onSubmit = handleSubmit((values) => {
+  patientStore.createPatient(values);
+  PatientList.push(values);
+  console.log(values);
+});
 
-// const { data } = await useFetch("/api/patients");
-// const patientList = data.value.data;
-
-// console.log(data.value);
+const id = defineInputBinds("id");
+const name = defineInputBinds("name");
+const email = defineInputBinds("email");
 </script>
 
 <template>
-  <div class="max-w-7xl mx-auto py-12">
-    <!-- <NuxtLink to="/patients/create" class="btn btn-accent mb-4"
-      >Add patient</NuxtLink
-    >
-    <div class="ring-2 ring-gray-200 rounded-md px-4 pb-4">
-      <ul role="list" class="divide-y divide-gray-100">
-        <li
-          v-for="person in patientList"
-          :key="person.email"
-          class="flex items-center justify-between gap-x-6 py-5"
-        >
-          <div class="flex min-w-0 gap-x-4">
-            <img
-              class="h-12 w-12 flex-none rounded-full bg-gray-50"
-              :src="person.imageUrl"
-              alt=""
-            />
-            <div class="min-w-0 flex-auto">
-              <p class="text-sm font-semibold leading-6 text-gray-900">
-                {{ person.name }}
-              </p>
-              <p class="mt-1 truncate text-xs leading-5 text-gray-500">
-                {{ person.email }}
-              </p>
-            </div>
-          </div>
-          <div class="flex gap-2">
-            <NuxtLink
-              :to="`/patients/${person.id}`"
-              class="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >Edit</NuxtLink
-            >
-            <NuxtLink
-              :to="`/patients/${person.id}`"
-              class="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >View</NuxtLink
-            >
-          </div>
+  <div class="max-w-7xl mx-auto py-12 px-4">
+    <div class="text-xl font-bold">Create/add Patient</div>
+    <form @submit="onSubmit" class="py-2 flex gap-4">
+      <div>
+        ID: <input type="number" v-bind="id" /> Name:
+        <input type="text" v-bind="name" /> email:
+        <input type="email" v-bind="email" />
+        <div>{{ errors.email }}</div>
+      </div>
+
+      <button class="btn">Submit</button>
+    </form>
+    <div>
+      <h1 class="text-xl font-bold">View Patient List</h1>
+      <ul class="text-black pt-2">
+        <li v-for="(patient, index) in PatientList" :key="index">
+          {{ index }}
+          name: {{ patient.email }} password: {{ patient.name }}
         </li>
       </ul>
-      <a
-        href="#"
-        class="flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
-        >View all</a
-      >
-    </div> -->
+    </div>
   </div>
 </template>
--- store/patient/patientStore.ts
+++ store/patient/patientStore.ts
@@ -25,13 +25,27 @@ export const usePatientStore = defineStore("PatientStore", {
     // },
 
     // Get list of all patients from server api patients.ts and store it in PatientList
-    async getPatientList() {
+    // async getPatientList() {
+    //   await $fetch("http://localhost:3000/api/patients", {
+    //     method: "GET",
+    //   })
+    //     .then((response: any) => {
+    //       console.log("getPatientList - ", response.data);
+    //       this.PatientList = response.data;
+    //     })
+    //     .catch((error) => {
+    //       throw error;
+    //     });
+    // },
+
+    async createPatient(values) {
       await $fetch("http://localhost:3000/api/patients", {
-        method: "GET",
+        method: "POST",
       })
         .then((response: any) => {
           console.log("getPatientList - ", response.data);
           this.PatientList = response.data;
+          this.PatientList.push(values);
         })
         .catch((error) => {
           throw error;
