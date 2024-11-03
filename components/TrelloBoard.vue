d7f5c54451b26f9aadcb81b4071de37d38a628b0
mahimaramgolam98 <mahima@circle-economy.com>
2024-11-03 14:41:07 +0400

    Trelloboard component

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { Column } from '~/types';
+import { nanoid } from "nanoid"
+
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: "Backlog",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Create marketing landing page",
+                createdAt: new Date()
+            },
+            {
+                id: nanoid(),
+                title: "task 2",
+                createdAt: new Date()
+            },
+            {
+                id: nanoid(),
+                title: "task 3",
+                createdAt: new Date()
+            }
+        ]
+    },
+    {
+        id: nanoid(),
+        title: "Selected for Devs",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Task 1 Column 2",
+                createdAt: new Date()
+            }
+        ]
+    },
+    {
+        id: nanoid(),
+        title: "in progress",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Task 1 Column 3",
+                createdAt: new Date()
+            }
+        ]
+    },
+    {
+        id: nanoid(),
+        title: "QA",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Task 1 Column 4",
+                createdAt: new Date()
+            }
+        ]
+    }
+]);
+
+</script>
+<template>
+    <div class="flex gap-4 overflow-x-auto items-start">
+        <div v-for="column in columns" :key="column.id" class="bg-gray-200 p-5 rounded min-w-[250px]">
+            <header>
+                {{ column.title }}
+            </header>
+            <p v-for="task in column.tasks" :key="task.id">
+                {{ task.title }}
+            </p>
+        </div>
+    </div>
+</template>
---
+++ pages/drag-and-drop.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="p-5 h-[100vh] bg-teal-600">
+        <TrelloBoard />
+    </div>
+</template>
